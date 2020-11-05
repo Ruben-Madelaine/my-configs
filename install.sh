103a59a8b55707a5b5961941729ceba23ae96f0c
Ruben Madelaine <madelaine.ruben@hotmail.com>
2020-11-05 13:00:56 +0100

    :arrow_up: Adding my Linux configuration files

---
+++ install.sh
@@ -0,0 +1,78 @@
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get autoremove
+
+sudo apt install chromium-browser
+sudo apt install python3.9
+
+
+sudo apt install tree
+sudo apt install docker
+sudo apt install i3
+sudo apt-get install terminator
+
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packagecloud.io/AtomEditor/atom/gpgkey -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main"
+sudo apt-get update
+sudo apt install atom
+
+
+sudo apt-get install dh-autoreconf libcurl4-gnutls-dev libexpat1-dev   gettext libz-dev libssl-dev
+sudo apt-get install asciidoc xmlto docbook2x
+sudo apt install git-all
+
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
+
+sudo apt install python3-pip
+pip3 install black
+sudo apt install unity-tweak-tool
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt upgrade
+sudo apt-get install sublime-text
+
+sudo apt install apt-transport-https curl gnupg
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
+------
+
+Shutter / Flameshot
+Kazam is my favorite screen recorder for Linux
+Calibre
+Simplenote
+Gnome Encfs Manager
+Weather
+Terminator
+CopyQ
+Stacer
+
+83. Simple Weather Indicator
+
+sudo snap install skype
+sudo apt-get install unity-tweak-tool
+
+sudo apt-get install qtqr
+
+Organize My Files
+
+
+
